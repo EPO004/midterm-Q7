1b215afcaf02aa07ad1f704e06116d4b32a72d36
EPO004 <119657120+EPO004@users.noreply.github.com>
2024-06-29 08:59:27 +0330

    Update evalpost_tb.v

--- Code/evalpost_tb.v
+++ Code/evalpost_tb.v
@@ -1,20 +1,15 @@
 `timescale 1ns/1ps
-
 module evalpost_tb;
-
     parameter LEN = 100; 
     parameter N = 16;   
-
     reg [8*LEN-1:0] infix;
     wire signed [N-1:0] result;
     wire overflow;
-
     evalpost #(.LEN(LEN), .N(N)) uut (
         .infix_expr(infix),
         .result(result),
         .overflow(overflow)
     );
-
     task padding;
         input integer str_len;
         begin
@@ -24,14 +19,11 @@ module evalpost_tb;
             end
         end
     endtask
-
     initial begin
         infix = "2 * 3 + (10 + 4 + 3) * -20 + (6 + 5)";
         padding(64);
         $display("Infix expression: %s", infix);
         #1000 $display("result: %d, overflow: %d", result, overflow);
-        
         $stop;
     end
-
 endmodule
